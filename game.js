f03213745c1f3f3ee6bf61e44d699ef8d845f9ef
Ming Hong <68141634+pingumingu@users.noreply.github.com>
2023-03-31 14:01:07 +1100

    Add prompt for user input and add function game() which allows user to play best of 5 against a computer

--- game.js
+++ game.js
@@ -33,21 +33,44 @@ function paperScissorsRock(playerSelection,computerSelection) {
     console.log(computerSelection);
 
     if (playerSelectionNumeric == computerSelection) {
-        return `It's a draw! Both you and the opponent played ${playerSelection}.`;
+        return [0,0,`It's a draw! Both you and the opponent played ${playerSelection}.`];
     } else if (playerSelectionNumeric == 1 && computerSelection == 2) {
-        return "You lose! Paper beats rock.";
+        return [0,1,"You lose! Paper beats rock."];
     } else if (playerSelectionNumeric == 1 && computerSelection == 3) {
-        return "You win! Rock beats scissors.";
+        return [1,0,"You win! Rock beats scissors."];
     } else if (playerSelectionNumeric == 2 && computerSelection == 1) {
-        return "You win! Paper beats rock.";
+        return [1,0,"You win! Paper beats rock."];
     } else if (playerSelectionNumeric == 2 && computerSelection == 3) {
-        return "You lose! Scissors beats paper.";
+        return [0,1,"You lose! Scissors beats paper."];
     } else if (playerSelectionNumeric == 3 && computerSelection == 1) {
-        return "You lose! Rock beats scissors.";
+        return [0,1,"You lose! Rock beats scissors."];
     } else {
-        return "You win! Scissors beats paper."
+        return [1,0,"You win! Scissors beats paper."]
     }
     
 }
 
-console.log(paperScissorsRock('rock',getComputerChoice()))
+function game() {
+    playerScore = 0;
+    computerScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+        selection = prompt("Play paper, scissors or rock?")
+        currentGame = paperScissorsRock(selection, getComputerChoice())
+        console.log(currentGame[2])
+        playerScore += currentGame[0]
+        computerScore += currentGame[1]
+        console.log(`The score is ${playerScore}-${computerScore}.`)
+    }
+
+    if (playerScore > computerScore) {
+        console.log(`The final score is ${playerScore}-${computerScore}. You win!`);
+    } else if (playerScore == computerScore) {
+        console.log(`The final score is ${playerScore}-${computerScore}. It's a draw!`)
+    } else {
+        console.log(`The final score is ${playerScore}-${computerScore}. You lose!`)
+    }
+
+}
+
+game()
